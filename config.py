826950b24c6fc702b42a0f4f546c2af6d473531b
daviddzxy <david.dzxy@gmail.com>
2020-10-21 22:22:30 +0200

    Analysis updated.

---
+++ config.py
@@ -0,0 +1,7 @@
+paths = {
+        "data_CT0": "/media/david/DATA/Covid-Data/COVID19_1110/studies/CT-0",
+        "data_CT1": "/media/david/DATA/Covid-Data/COVID19_1110/studies/CT-1",
+        "data_CT2": "/media/david/DATA/Covid-Data/COVID19_1110/studies/CT-2",
+        "data_CT3": "/media/david/DATA/Covid-Data/COVID19_1110/studies/CT-3",
+        "data_CT4": "/media/david/DATA/Covid-Data/COVID19_1110/studies/CT-4"
+        }
