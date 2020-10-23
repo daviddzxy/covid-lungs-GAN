5dc5fe29e754e301b4e75fbe3136e4137bb7c525
daviddzxy <david.dzxy@gmail.com>
2020-10-23 20:28:03 +0200

    Add logging to preprocessing.

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,9 +1,9 @@
+import logging
 import nibabel as nib
 import SimpleITK as sitk
 import os
 import numpy as np
 import scipy
-import matplotlib.pyplot as plt
 from lungmask import mask
 
 
@@ -15,9 +15,10 @@ def resample(image, pix_dims, new_spacing=(1, 1, 8)):
     return resampled_image
 
 
-def preprocess():
+def _preprocess():
     for key, path in config.data_paths.items():
         for f in os.listdir(path):
+            logging.info("Preprocessing of file {} started".format(f))
             sitk_img = sitk.ReadImage(os.path.join(path, f))
             nib_img = nib.load(os.path.join(path, f))  # mask function from lungmask module works with sitk objects
             # only, might implement adapter for this object so the image does not have to be loaded two times
@@ -26,9 +27,19 @@ def preprocess():
             resampled_vol = resample(nib_img.get_fdata().transpose(1, 0, 2), nib_img.header['pixdim'][1:4])
             resampled_segmented_vol = resample(segmented_vol.transpose(1, 2, 0), nib_img.header['pixdim'][1:4])
             resampled_masked_vol = np.where(resampled_segmented_vol == 0, resampled_segmented_vol, resampled_vol)
+            np.save(os.path.join(config.preprocessed_data_paths[key], f.split(".")[0] + ".npy"), resampled_masked_vol)
+            logging.info("Preprocessing of file {}  finished".format(f))
+
 
 if __name__ == '__main__':
-    os.sys.path.append('../')
     import config
-    preprocess()
+    os.sys.path.append(config.project_root)
+    logging.basicConfig(filename=os.path.join(config.log_dir, "preprocessing_logs.log"),
+                        format='%(asctime)s %(levelname)-8s %(message)s',
+                        datefmt='%d-%m-%Y %H:%M:%S',
+                        filemode='w',
+                        level=logging.INFO,
+                        force=True)
+    _preprocess()
+
 
