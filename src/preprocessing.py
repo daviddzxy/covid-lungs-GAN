7e7559978ea5e69f489ed979827a300e0f3ac172
daviddzxy <david.dzxy@gmail.com>
2020-10-29 01:40:27 +0100

    Added more transformations that will be used during data loading. Preprocessing_utils have been removed and rewritten as transformations. Preprocessing.py was rewritten to use the new api from transformations.

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -2,44 +2,42 @@ import logging
 import nibabel as nib
 import SimpleITK as sitk
 import os
+import pickle
+import config
 import numpy as np
-import scipy
 from lungmask import mask
-
-
-def resample(image, pix_dims, new_spacing=(1, 1, 8)):
-    resize_factor = pix_dims / new_spacing
-    new_shape = np.round(image.shape * resize_factor)
-    real_resize_factor = new_shape / image.shape
-    resampled_image = scipy.ndimage.interpolation.zoom(image, real_resize_factor, mode='nearest')
-    return resampled_image
-
-
-def _preprocess():
-    for key, path in config.data_paths.items():
-        for f in os.listdir(path):
-            logging.info("Preprocessing of file {} started".format(f))
-            sitk_img = sitk.ReadImage(os.path.join(path, f))
-            nib_img = nib.load(os.path.join(path, f))  # mask function from lungmask module works with sitk objects
-            # only, might implement adapter for this object so the image does not have to be loaded two times
-
-            segmented_vol = mask.apply(sitk_img, batch_size=1)
-            resampled_vol = resample(nib_img.get_fdata().transpose(1, 0, 2), nib_img.header['pixdim'][1:4])
-            resampled_segmented_vol = resample(segmented_vol.transpose(1, 2, 0), nib_img.header['pixdim'][1:4])
-            resampled_masked_vol = np.where(resampled_segmented_vol == 0, resampled_segmented_vol, resampled_vol)
-            np.save(os.path.join(config.preprocessed_data_paths[key], f.split(".")[0] + ".npy"), resampled_masked_vol)
-            logging.info("Preprocessing of file {}  finished".format(f))
-
+from src.transformations import ResampleVolume, ApplyMask
 
 if __name__ == '__main__':
-    import config
     os.sys.path.append(config.project_root)
-    logging.basicConfig(filename=os.path.join(config.log_dir, "preprocessing_logs.log"),
+    logging.basicConfig(filename=os.path.join(config.log_dir, 'preprocessing_logs.log'),
                         format='%(asctime)s %(levelname)-8s %(message)s',
                         datefmt='%d-%m-%Y %H:%M:%S',
                         filemode='w',
                         level=logging.INFO,
                         force=True)
-    _preprocess()
-
+    
+    apply_mask = ApplyMask()
+    resample = ResampleVolume(new_spacing=(1, 1, 8))
+    for key, path in config.data_paths.items():
+        for f in os.listdir(path):
+            logging.info('Preprocessing of file {} started'.format(f))
+            sitk_img = sitk.ReadImage(os.path.join(path, f))
+            # mask function from lungmask module works with sitk objects only,
+            # I might implement adapter for this object so the image does not have to be loaded two times
+            nib_img = nib.load(os.path.join(path, f))
+            segmented_vol = mask.apply(sitk_img, batch_size=1)
+            resampled_vol, resize_factor = resample(
+                nib_img.get_fdata().transpose(1, 0, 2), nib_img.header['pixdim'][1:4]
+            )
+            resampled_segmented_vol, _ = resample(
+                segmented_vol.transpose(1, 2, 0), nib_img.header['pixdim'][1:4]
+            )
+            logging.info('Volume shape before resampling: {}, volume shape after resampling {}'.format(
+                nib_img.get_fdata().shape, resampled_vol.shape)
+            )
+            resampled_masked_vol = apply_mask(resampled_vol, mask=resampled_segmented_vol)
+            with open(os.path.join(config.preprocessed_data_paths[key], f.split('.')[0] + '.pkl'), 'wb') as handle:
+                pickle.dump({'data': resampled_masked_vol, 'resize_factor': resize_factor}, handle)
+            logging.info('Preprocessing of file {} finished'.format(f))
 
---
+++ src/transformations.py
@@ -0,0 +1,90 @@
+import numpy as np
+import scipy
+
+
+class PadVolume:
+    """
+    Pads volume with zeros.
+    """
+
+    def __init__(self, dimensions=(512, 512, 58)):
+        """
+        :param dimensions: Output dimensions(H_out, W_out, D_out) of padded volume.
+        """
+        assert len(dimensions.shape) == 3
+        self.dimensions = dimensions
+
+    def __call__(self, volume):
+        """
+        :param volume: Volume with size (H_in, W_in, D_in) to be padded.
+        :return: Returns padded volume with zeros with size H_out, W_out, D_out).
+        """
+        pad1, pad2, pad3 = self.dimensions[0] - volume.shape[0], \
+                           self.dimensions[1] - volume.shape[1], \
+                           self.dimensions[2] - volume.shape[2],
+
+        pad1, pad2, pad3 = int(pad1 / 2), int(pad2 / 2), int(pad3 / 2)
+        odd1, odd2, odd3 = 0, 0, 0
+
+        if pad1 // 2:
+            odd1 = 1
+        if pad2 // 2:
+            odd2 = 1
+        if pad3 // 2:
+            odd3 = 1
+
+        return np.pad(volume, ((pad1, pad1 + odd1), (pad2, pad2 + odd2), (pad3, pad3 + odd3)), mode='constant')
+
+
+class GetMiddleLungSlice:
+    """
+    Returns middle slice of segmented lung volume.
+    """
+
+    def __call__(self, volume):
+        """
+        :param volume: Volume of input size (H_in, W_in, D_in).
+        :return: Returns middle slice(H_out, W_out) of segmented lung volume of size (H_in, W_in, D_in).
+        """
+        # Remove all elements from rows and columns that contain zeroes only.
+        lung_slices = volume[:, :, ~(volume == 0).all(axis=(0, 1))]
+        return lung_slices[:, :, lung_slices.shape[2] // 2]
+
+
+class ResampleVolume:
+    """
+    Resample volume to different voxel_spacing.
+    Voxel spacing (1, 1, 1) means that one pixel represents volume of 1x1x1mm in real world.
+    """
+    def __init__(self, new_spacing):
+        """
+        :param new_spacing: new_spacing has to be a tuple of length 3.
+        """
+        self.new_spacing = new_spacing
+
+    def __call__(self, volume, current_spacing):
+        """
+        :param volume: Volume to be resampled of size (H_in, W_in, D_in).
+        :param current_spacing: Current voxel spacing values. current spacing has to be a tuple of length 3.
+        :return: Returns volume with new size of (H_out, W_out, D_out)
+                 and tuple of length 3 with corresponding resize factors. 
+        """
+        resize_factor = current_spacing / self.new_spacing
+        new_shape = np.round(volume.shape * resize_factor)
+        real_resize_factor = new_shape / volume.shape
+        resampled_volume = scipy.ndimage.interpolation.zoom(volume, real_resize_factor, mode='nearest')
+        return resampled_volume, real_resize_factor
+
+
+class ApplyMask:
+    """
+    Applies mask to volume.
+    """
+    def __call__(self, volume, mask):
+        """
+        :param volume: Volume of size (H_in, W_in, D_in) to be masked.
+        :param mask: Mask of size (H_in, W_in, D_in), which contains nonzero elements,
+                     which will be used to keep relevant infromation in volume.
+        :return:
+        """
+        return np.where(mask == 0, mask, volume)
