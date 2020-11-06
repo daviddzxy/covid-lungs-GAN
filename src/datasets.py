f4c89f9cbb55484d96b6472b4a9f99e4a0da38ca
daviddzxy <david.dzxy@gmail.com>
2020-11-06 20:52:07 +0100

    Fixed bug.

--- src/datasets.py
+++ src/datasets.py
@@ -15,9 +15,10 @@ class CycleGanDataset(Dataset):
         self.to_tensor = ToTensor()
 
     def __getitem__(self, index):
-        file_handler_CT0 = open(self.paths_CT0[index % len(self.paths_CT0)], 'rb')
-        file_handler_CT3 = open(self.paths_CT3[index % len(self.paths_CT3)], 'rb')
-        image_CT0, image_CT3 = pickle.load(file_handler_CT0)['data'], pickle.load(file_handler_CT3)['data']
+        file_handler_CT0 = open(os.path.join(config.preprocessed_data_paths['CT0'], self.paths_CT0[index % len(self.paths_CT0)]), 'rb')
+        file_handler_CT3 = open(os.path.join(config.preprocessed_data_paths['CT3'], self.paths_CT3[index % len(self.paths_CT3)]), 'rb')
+        image_CT0 = pickle.load(file_handler_CT0)['data']
+        image_CT3 = pickle.load(file_handler_CT3)['data']
         image_CT0, image_CT3 = self.pad_volume(image_CT0), self.pad_volume(image_CT3)
         image_CT0, image_CT3 = self.get_middle_lung_slice(image_CT0), self.get_middle_lung_slice(image_CT3)
         image_CT0, image_CT3 = self.normalize(image_CT0), self.normalize(image_CT3)
@@ -27,4 +28,4 @@ class CycleGanDataset(Dataset):
         return image_CT0, image_CT3
 
     def __len__(self):
-        return max(self.paths_CT0, self.paths_CT3)
+        return max(len(self.paths_CT0), len(self.paths_CT3))
