ac9afa2d25dcc39eb09b0ea91075652c90f1898d
daviddzxy <david.dzxy@gmail.com>
2020-11-12 01:51:09 +0100

    Split Conv class and Generator class to separate files. Add one upconv layer to generator to meet shape of original image.

--- src/generators.py
+++ src/generators.py
@@ -1,77 +1,23 @@
-from torch import nn, cat
-
-
-class Conv(nn.Module):
-    def __init__(self, in_channels, out_channels, norm=True, activ='relu'):
-        super().__init__()
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
-        if activ == 'relu':
-            self.activ = nn.ReLU(inplace=True)
-        else:
-            self.activ = nn.Tanh()
-        self.norm = nn.BatchNorm2d(out_channels) if norm else None
-
-    def forward(self, x):
-        x = self.conv(x)
-        x = self.activ(x)
-        x = self.norm(x) if self.norm else x
-        return x
-
-
-class DownSampling(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super().__init__()
-        self.block = nn.Sequential(
-            Conv(in_channels, out_channels),
-            Conv(out_channels, out_channels),
-            nn.MaxPool2d(2)
-        )
-
-    def forward(self, x):
-        return self.block(x)
-
-
-class UpSampling(nn.Module):
-    def __init__(self, in_channels, out_channels, outermost=False):
-        super().__init__()
-        self.block = nn.Sequential(
-            Conv(in_channels, out_channels),
-            Conv(out_channels, out_channels) if outermost is False else Conv(
-                                                                            out_channels,
-                                                                            out_channels,
-                                                                            norm=False,
-                                                                            activ='tanh')
-        )
-        if outermost is False:
-            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-        else:
-            self.up = None
-
-    def forward(self, x1, x2):
-        if x2 is not None:
-            assert x1.shape[1] == x2.shape[1]
-            x1 = cat((x1, x2), dim=1)
-
-        x = self.block(x1)
-        x = self.up(x) if self.up is not None else x
-        return x
+from torch import nn
+from network_parts import DownSampling, UpSampling
 
 
 class UnetGenerator2D(nn.Module):
     def __init__(self, filters):
         super().__init__()
-        self.ds1 = DownSampling(in_channels=1, out_channels=filters)
-        self.ds2 = DownSampling(in_channels=filters, out_channels=filters*2)
-        self.ds3 = DownSampling(in_channels=filters*2, out_channels=filters*4)
-        self.ds4 = DownSampling(in_channels=filters*4, out_channels=filters*8)
-        self.ds5 = DownSampling(in_channels=filters*8, out_channels=filters*16)
-        self.ds6 = DownSampling(in_channels=filters*16, out_channels=filters*32)
-        self.up1 = UpSampling(in_channels=filters*32, out_channels=filters*16)
-        self.up2 = UpSampling(in_channels=filters*2*16, out_channels=filters*8)
-        self.up3 = UpSampling(in_channels=filters*2*8, out_channels=filters*4)
-        self.up4 = UpSampling(in_channels=filters*2*4, out_channels=filters*2)
-        self.up5 = UpSampling(in_channels=filters*2*2, out_channels=filters)
-        self.up6 = UpSampling(in_channels=filters*2, out_channels=1, outermost=True)
+        self.ds1 = DownSampling(in_channels=1, out_channels=filters, kernel_size=3, stride=1, padding=1)
+        self.ds2 = DownSampling(in_channels=filters, out_channels=filters*2, kernel_size=3, stride=1, padding=1)
+        self.ds3 = DownSampling(in_channels=filters*2, out_channels=filters*4, kernel_size=3, stride=1, padding=1)
+        self.ds4 = DownSampling(in_channels=filters*4, out_channels=filters*8, kernel_size=3, stride=1, padding=1)
+        self.ds5 = DownSampling(in_channels=filters*8, out_channels=filters*16, kernel_size=3, stride=1, padding=1)
+        self.ds6 = DownSampling(in_channels=filters*16, out_channels=filters*32, kernel_size=3, stride=1, padding=1)
+        self.up1 = UpSampling(in_channels=filters*32, out_channels=filters*16, kernel_size=3, stride=1, padding=1)
+        self.up2 = UpSampling(in_channels=filters*2*16, out_channels=filters*8, kernel_size=3, stride=1, padding=1)
+        self.up3 = UpSampling(in_channels=filters*2*8, out_channels=filters*4, kernel_size=3, stride=1, padding=1)
+        self.up4 = UpSampling(in_channels=filters*2*4, out_channels=filters*2, kernel_size=3, stride=1, padding=1)
+        self.up5 = UpSampling(in_channels=filters*2*2, out_channels=filters, kernel_size=3, stride=1, padding=1)
+        self.up6 = UpSampling(in_channels=filters*2, out_channels=filters, kernel_size=3, stride=1, padding=1)
+        self.up7 = UpSampling(in_channels=filters, out_channels=1, kernel_size=3, stride=1, padding=1, outermost=True)
 
     def forward(self, x):
         down1 = self.ds1(x)
@@ -86,7 +32,9 @@ class UnetGenerator2D(nn.Module):
         up4 = self.up4(up3, down3)
         up5 = self.up5(up4, down2)
         up6 = self.up6(up5, down1)
-        return up6
+        up7 = self.up7(up6, None)
+
+        return up7
 
 
 
---
+++ src/network_parts.py
@@ -0,0 +1,63 @@
+from torch import nn, cat
+
+
+class Conv(nn.Module):
+    #TODO add Instance norm option
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding, norm=True, activ='relu'):
+        super().__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding)
+        if activ == 'relu':
+            self.activ = nn.ReLU(inplace=True)
+        elif activ == 'tanh':
+            self.activ = nn.Tanh()
+        else:
+            self.activ = None
+        self.norm = nn.BatchNorm2d(out_channels) if norm else None
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.activ(x) if self.activ else x
+        x = self.norm(x) if self.norm else x
+        return x
+
+
+class DownSampling(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding):
+        super().__init__()
+        self.block = nn.Sequential(
+            Conv(in_channels, out_channels, kernel_size, stride, padding),
+            Conv(out_channels, out_channels, kernel_size, stride, padding),
+            nn.MaxPool2d(2)
+        )
+
+    def forward(self, x):
+        return self.block(x)
+
+
+class UpSampling(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding, outermost=False):
+        super().__init__()
+        self.block = nn.Sequential(
+            Conv(in_channels, out_channels, kernel_size, stride, padding),
+            Conv(out_channels, out_channels, kernel_size, stride, padding) if outermost is False else Conv(
+                                                                            out_channels,
+                                                                            out_channels,
+                                                                            kernel_size,
+                                                                            stride,
+                                                                            padding,
+                                                                            norm=False,
+                                                                            activ='tanh')
+        )
+        if outermost is False:
+            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        else:
+            self.up = None
+
+    def forward(self, x1, x2):
+        if x2 is not None:
+            assert x1.shape[1] == x2.shape[1]
+            x1 = cat((x1, x2), dim=1)
+
+        x = self.block(x1)
+        x = self.up(x) if self.up is not None else x
+        return x
