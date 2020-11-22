5738d66f4a107841e2b9ce06001ce6c488f1f90b
daviddzxy <david.dzxy@gmail.com>
2020-11-22 16:16:09 +0100

    Move pad_volume, get_middle_lung_slice from on the fly preprocessing in dataset to preprocessing script, which serlializes preprocessed images.

--- src/datasets.py
+++ src/datasets.py
@@ -1,7 +1,7 @@
 import os
 import config
 import pickle
-from src.transformations import PadVolume, GetMiddleLungSlice, Normalize, ToTensor
+from src.transformations import Normalize, ToTensor
 from torch.utils.data import Dataset
 
 
@@ -9,8 +9,6 @@ class CycleGanDataset(Dataset):
     def __init__(self):
         self.paths_CT0 = os.listdir(config.preprocessed_data_paths['CT0'])
         self.paths_CT3 = os.listdir(config.preprocessed_data_paths['CT3'])
-        self.pad_volume = PadVolume((512, 512, 64))
-        self.get_middle_lung_slice = GetMiddleLungSlice()
         self.normalize = Normalize()
         self.to_tensor = ToTensor()
 
@@ -19,8 +17,6 @@ class CycleGanDataset(Dataset):
         file_handler_CT3 = open(os.path.join(config.preprocessed_data_paths['CT3'], self.paths_CT3[index % len(self.paths_CT3)]), 'rb')
         image_CT0 = pickle.load(file_handler_CT0)['data']
         image_CT3 = pickle.load(file_handler_CT3)['data']
-        image_CT0, image_CT3 = self.pad_volume(image_CT0), self.pad_volume(image_CT3)
-        image_CT0, image_CT3 = self.get_middle_lung_slice(image_CT0), self.get_middle_lung_slice(image_CT3)
         image_CT0, image_CT3 = self.normalize(image_CT0), self.normalize(image_CT3)
         image_CT0, image_CT3 = self.to_tensor(image_CT0), self.to_tensor(image_CT3)
         file_handler_CT0.close()
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -4,11 +4,12 @@ import SimpleITK as sitk
 import os
 import pickle
 import config
+from matplotlib import pyplot as plt
 from lungmask import mask
-from transformations import ResampleVolume, ApplyMask
+from transformations import ResampleVolume, ApplyMask, PadVolume, GetMiddleLungSlice
 
 os.sys.path.append(config.project_root)
-logging.basicConfig(filename=os.path.join(config.log_dir, 'preprocessing_logs.log'),
+logging.basicConfig(filename=os.path.join(config.preprocessing_logs, 'preprocessing_logs.log'),
                     format='%(asctime)s %(levelname)-8s %(message)s',
                     datefmt='%d-%m-%Y %H:%M:%S',
                     filemode='w',
@@ -16,18 +17,22 @@ logging.basicConfig(filename=os.path.join(config.log_dir, 'preprocessing_logs.lo
                     force=True)
 
 apply_mask = ApplyMask()
+pad_volume = PadVolume(config.padding_shape)
+get_middle_lung_slice = GetMiddleLungSlice()
+
 resample = ResampleVolume(new_spacing=(1, 1, 8))
 for key, path in config.data_paths.items():
     for f in os.listdir(path):
         logging.info('Preprocessing of file {} started'.format(f))
         sitk_img = sitk.ReadImage(os.path.join(path, f))
-        # mask function from lungmask module works with sitk objects only,
-        # I might implement adapter for this object so the image does not have to be loaded two times
         nib_img = nib.load(os.path.join(path, f))
         img = nib_img.get_fdata().transpose(1, 0, 2)
         img_s = mask.apply(sitk_img, batch_size=1, noHU=False).transpose(1, 2, 0)
-        img = apply_mask(img, img_s)
         img, resize_factor = resample(img, nib_img.header['pixdim'][1:4])
+        img_s, _ = resample(img_s, nib_img.header['pixdim'][1:4])
+        img = apply_mask(img, img_s)
+        img, img_s = pad_volume(img), pad_volume(img_s)
+        img, img_s = get_middle_lung_slice(img), get_middle_lung_slice(img_s)
         logging.info('Volume shape before resampling: {}, volume shape after resampling {}'.format(
             nib_img.get_fdata().shape, img.shape)
         )
