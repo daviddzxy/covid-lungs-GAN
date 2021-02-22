9d1646f3663b80d8f766488d6f14a015afe08569
daviddzxy <david.dzxy@gmail.com>
2021-02-22 17:50:08 +0100

    Add cgan dataset.

--- src/datasets.py
+++ src/datasets.py
@@ -1,5 +1,6 @@
 import os
 import pickle
+import random
 from torch.utils.data import Dataset
 from transformations import ToTensor
 
@@ -9,7 +10,7 @@ class CycleGanDataset(Dataset):
         self.dir_A = images_A
         self.dir_B = images_B
         self.files_A = os.listdir(images_A)
-        self.files_B = os.listdir(images_B, )
+        self.files_B = os.listdir(images_B)
         self.mask = mask
         self.rotate = rotation
         self.crop = crop
@@ -19,10 +20,10 @@ class CycleGanDataset(Dataset):
     def __getitem__(self, index):
         file_handler_A = open(os.path.join(self.dir_A, self.files_A[index % len(self.files_A)]), "rb")
         file_handler_B = open(os.path.join(self.dir_B, self.files_B[index % len(self.files_B)]), "rb")
-        A = pickle.load(file_handler_A)
-        B = pickle.load(file_handler_B)
-        image_A, image_B = A["data"], B["data"]
-        mask_A, mask_B = A["mask"], B["mask"]
+        file_A = pickle.load(file_handler_A)
+        file_B = pickle.load(file_handler_B)
+        image_A, image_B = file_A["data"], file_B["data"]
+        mask_A, mask_B = file_A["mask"], file_B["mask"]
 
         if self.normalize:
             image_A, image_B = self.normalize(image_A), self.normalize(image_B)
@@ -44,3 +45,35 @@ class CycleGanDataset(Dataset):
 
     def __len__(self):
         return max(len(self.files_A), len(self.files_B))
+
+
+class CganDataset(Dataset):
+    def __init__(self, images, mask, max_rotation=None, rotation=None, crop=None, normalize=None):
+        self.dir = images
+        self.files = os.listdir(images)
+        self.mask = mask
+        self.max_rotation = max_rotation
+        self.rotate = rotation
+        self.crop = crop
+        self.normalize = normalize
+        self.to_tensor = ToTensor()
+
+    def __getitem__(self, index):
+        file_handler = open(os.path.join(self.dir, self.files[index]), "rb")
+        file = pickle.load(file_handler)
+        image, mask = file["data"], file["mask"]
+        if self.normalize:
+            image = self.normalize(image)
+
+        masked_image = mask(mask, image)
+
+        if self.rotate:
+            rand_angle = random.randint(-self.max_rotation, self.max_rotation)
+            image, masked_image = self.rotate(image, rand_angle), self.rotate(masked_image, rand_angle)
+
+        if self.crop:
+            image, masked_image = self.crop(image), self.crop(masked_image)
+
+        image, masked_image = self.to_tensor(image), self.to_tensor(masked_image)
+
+        return image, masked_image
